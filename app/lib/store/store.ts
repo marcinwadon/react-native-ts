837c11bccc58b95524ce831ea3c4b39c8b8ee5f5
Marcin Wadon <marcin@wadon.net>
2018-05-09 18:22:02 +0200

    Store fix

--- app/lib/store/store.ts
+++ app/lib/store/store.ts
@@ -3,14 +3,10 @@ import { AnyAction, applyMiddleware, createStore, Middleware } from 'redux'
 import { composeWithDevTools } from 'remote-redux-devtools'
 import { reducer } from './reducer'
 
-const REDUX_DEVTOOLS_MONITORING_ENABLED = true
-
 // TODO: temporary, till real middleware needed
-const empty = () => (next: Dispatch<AnyAction>) => (action: AnyAction) => next(action)
+const empty = () => (next: Dispatch<AnyAction>) => next
 
 const middlewares: Middleware[] = [empty]
-const enhancer = composeWithDevTools({
-  realtime: REDUX_DEVTOOLS_MONITORING_ENABLED,
-})(applyMiddleware(...middlewares) as any) as any // tslint:disable-line no-any
+const enhancer = composeWithDevTools(applyMiddleware(...middlewares) as any) as any // tslint:disable-line no-any
 
 export const store = createStore(reducer, enhancer)
